4ffa1eeafe83aca2b4f6c4ab906c06836dec92e7
glog <bglogowski@gmail.com>
2021-07-14 19:40:46 -0700

    GitHub commit

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
+import getopt
+import hashlib
 import os
 import sys
-import getopt
-
 import threading
+from timeit import default_timer as timer
 
-import boto3
 import boto3.s3.transfer
 import botocore.exceptions
 
-import hashlib
 
-from timeit import default_timer as timer
+def hash_file(file_path) -> str:
+    file_buffer: int = 65536
 
-print(os.environ)
-
-exclude_list: list[str] = ['.DS_Store']
-
-
-def hash_file(file):
-    BUF_SIZE: int = 65536
     sha256 = hashlib.sha256()
 
-    with open(file, 'rb') as f:
+    with open(file_path, 'rb') as f:
         while True:
-            data = f.read(BUF_SIZE)
+            data = f.read(file_buffer)
             if not data:
                 break
             sha256.update(data)
@@ -31,25 +24,21 @@ def hash_file(file):
     return sha256.hexdigest()
 
 
-def s3_add_metadata(bucket, obj, metadata):
-    s3 = boto3.resource('s3')
-    s3_object = s3.Object(bucket, obj)
-
-    s3_object.metadata.update(metadata)
-    s3_object.copy_from(CopySource={'Bucket': bucket, 'Key': obj}, Metadata=s3_object.metadata,
-                        MetadataDirective='REPLACE')
-
-
-def s3_upload(file, bucket, obj, metadata):
+def s3_upload(file, bucket, obj, metadata) -> float:
     client = boto3.client('s3')
     transfer = boto3.s3.transfer.S3Transfer(client)
-    size = float(os.path.getsize(file))
+
+    size: float = float(os.path.getsize(file))
 
     start = timer()
-    transfer.upload_file(file, bucket, obj, extra_args=metadata, callback=_progress(file, size, 'Upload'))
+    transfer.upload_file(file,
+                         bucket,
+                         obj,
+                         extra_args=metadata,
+                         callback=_progress(file, size, 'Upload'))
     end = timer()
 
-    print("Upload completed in " + str(end - start) + " seconds.")
+    return end - start
 
 
 def _progress(filename, size, ops):
@@ -68,84 +57,64 @@ def _progress(filename, size, ops):
             nonlocal _msg_count
             _seen_so_far += bytes_amount
             percentage = (_seen_so_far / _size) * 100
-            # logging.info(
+
             if _msg_count % _msg_throttle == 0:
-                print("%s: %s  %s / %s  (%.2f%%)" % (
-                    _ops, _filename, _seen_so_far, round(_size),
-                    percentage))
+                print(f"{_ops}: {_filename}  {_seen_so_far} / {round(_size)}  ({percentage:.2f}%)")
 
             _msg_count += 1
 
     return call
 
 
-def get_files(directory):
-    return local_get_files(directory)
+def fs_get_files(directory):
+    exclude_list: set[str] = {".DS_Store"}
+    catalog: dict[str, str] = {}
 
+    for (path, dirs, files) in os.walk(directory):
+        if not any(x in files for x in exclude_list):
+            for f in files:
+                catalog[f] = path + "/" + f
 
-def local_get_files(directory):
-    print("Getting files...")
-    catalog = {}
-
-    for (dir_path, dir_names, filenames) in os.walk(directory):
-        if not any(item in filenames for item in exclude_list):
-            for f in filenames:
-                catalog[f] = dir_path + '/' + f
     return catalog
 
 
-def put_files(catalog, bucket):
-    return s3_put_files(catalog, bucket)
-
-
 def s3_put_files(catalog, bucket):
-    s3 = boto3.resource('s3')
-
-    # s3 = boto3.client('s3')
+    s3 = boto3.client('s3')
 
     for file in catalog:
 
         sha256 = hash_file(catalog[file])
-        m = {"Metadata": {'sha256': sha256}}
+        m = {"Metadata": {"sha256": sha256}}
 
         try:
-            s3.Object(bucket, file).load()
-            metadata = s3.head_object(Bucket=bucket, Key=file)['Metadata']
+            s3_metadata = s3.head_object(Bucket=bucket, Key=file)['Metadata']
 
         except botocore.exceptions.ClientError as e:
             if e.response['Error']['Code'] == "404":
                 print(file + " does not exist in " + bucket)
 
-                start = timer()
-
-                s3_upload(catalog[file], bucket, file, m)
-                end = timer()
-                print("Upload completed in " + str(end - start) + " seconds.")
+                elapsed_time = s3_upload(catalog[file], bucket, file, m)
+                print("Upload completed in " + str(elapsed_time) + " seconds.")
                 break
 
         else:
 
-            metadata = boto3.client('s3').head_object(Bucket=bucket, Key=file)['Metadata']
+            s3_hash = s3_metadata['sha256']
 
-            old_hash = metadata['sha256']
-            # new_hash = hash_file(catalog[file])
-            new_hash = sha256
-
-            if old_hash == new_hash:
+            if s3_hash == sha256:
                 print(file + " object exists in S3 with matching hash. Skipping...")
             else:
-                print(file + " hash (" + new_hash + ") doesn't match S3 object (" + old_hash + "). Uploading again...")
+                print(file + " hash (" + sha256 + ") doesn't match S3 object (" + s3_hash + ").")
+                print("Uploading again...")
 
-                start = timer()
-                s3_upload(catalog[file], bucket, file, m)
-                end = timer()
-                print("Upload completed in " + str(end - start) + " seconds.")
+                elapsed_time = s3_upload(catalog[file], bucket, file, m)
+                print("Upload completed in " + str(elapsed_time) + " seconds.")
                 break
 
 
 def main(argv):
-    directory = ''
-    bucket = ''
+    directory: str = ""
+    bucket: str = ""
 
     try:
         opts, args = getopt.getopt(argv, "hb:d:", ["bucket=", "directory="])
@@ -166,10 +135,12 @@ def main(argv):
         elif opt in ("-b", "--bucket"):
             bucket = arg
 
-        print(bucket)
-        files = get_files(directory)
-        put_files(files, bucket)
+    files: dict[str, str] = fs_get_files(directory)
+    assert isinstance(bucket, str)
+
+    return s3_put_files(files, bucket)
 
 
 if __name__ == "__main__":
     main(sys.argv[1:])
+    sys.exit()
