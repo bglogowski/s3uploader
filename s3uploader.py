f1d77808c3ab9da3abd99736210ab20d433044cd
glog <bglogowski@gmail.com>
2021-07-29 13:02:56 -0700

    backported changes from package

--- s3uploader.py
+++ s3uploader.py
@@ -1,31 +1,4 @@
-"""
-Amazon S3 file uploader script
-======================
 
-Uploads local files to S3 while optionally retaining the
-local directory structure.
-
-S3 Folder Structure
--------------------
-S3 is not a filesystem, it is an object store. Therefore,
-the default behavior is simply to upload files as objects
-into a bucket using a flat namespace. However, owing to
-the complexity of real world data, the script will optionally
-organize the data in S3 using the local directory structure.
-
-File integrity checks
--------------------
-S3 Etags cannot be guaranteed to be MD5 hashes of the original
-file, such as when multi-part uploads are used to create the
-object. Therefore a cryptographic hash of the original file
-will be stored as metadata of the S3 object.
-
-SHA-256 was chosen because MD5 and SHA-1 are compromised and
-therefore unreliable measures of file integrity. SHA-256 is
-also reasonably performant as compared to alternative hash
-algorithms.
-
-"""
 
 import getopt
 import hashlib
@@ -77,10 +50,8 @@ class LocalFile(object):
         self._hash = None
         self._metadata = None
 
-    @classmethod
-    def from_csv(cls, text: str):
-        name, path, base_path = [v.strip() for v in text.split(',')]
-        return cls(name, path, base_path)
+    def _identify(self):
+        return self.__class__.__name__ + "." + sys._getframe(1).f_code.co_name
 
     @staticmethod
     def exists(path: str) -> bool:
@@ -99,7 +70,10 @@ class LocalFile(object):
                     break
                 sha256.update(data)
 
-        log.debug(str(__class__.__name__) + ".sha256 = " + sha256.hexdigest())
+        log.debug(__class__.__name__ + "." +
+                  sys._getframe().f_code.co_name + " = " +
+                  sha256.hexdigest())
+
         return sha256.hexdigest()
 
     @property
@@ -109,7 +83,7 @@ class LocalFile(object):
     @base_path.setter
     def base_path(self, value):
         self._base_path = value
-        log.debug(str(self.__class__.__name__) + ".base_path = " + self._base_path)
+        log.debug(self._identify() + " = " + self._base_path)
 
     @property
     def full_path(self) -> str:
@@ -119,7 +93,7 @@ class LocalFile(object):
     def hash(self) -> str:
         if self._hash is None:
             self._hash = self.sha256(self.full_path)
-            log.debug(str(self.__class__.__name__) + ".hash = " + self._hash)
+            log.debug(self._identify() + " = " + self._hash)
         return self._hash
 
     @property
@@ -135,7 +109,7 @@ class LocalFile(object):
     @name.setter
     def name(self, value):
         self._name = value
-        log.debug(str(self.__class__.__name__) + ".name = " + self._name)
+        log.debug(self._identify() + " = " + self._name)
 
     @property
     def path(self):
@@ -143,17 +117,20 @@ class LocalFile(object):
 
     @path.setter
     def path(self, value):
+
+
+
         self._path = value
-        log.debug(str(self.__class__.__name__) + ".path = " + self._path)
+        log.debug(self._identify() + " = " + self._path)
 
         self._full_path = self.path + "/" + self.name
-        log.debug(str(self.__class__.__name__) + ".full_path = " + self._full_path)
+        log.debug(self.__class__.__name__ + ".full_path = " + self._full_path)
 
         relative_path = self.full_path.replace(self.base_path, "")
         relative_path = re.sub(r'^/', '', relative_path)
         relative_path = re.sub(r'^\./', '', relative_path)
         self._relative_path = relative_path
-        log.debug(str(self.__class__.__name__) + ".relative_path = " + self._relative_path)
+        log.debug(self.__class__.__name__ + ".relative_path = " + self._relative_path)
 
     @property
     def relative_path(self) -> str:
@@ -166,13 +143,13 @@ class LocalFile(object):
     @s3key.setter
     def s3key(self, value):
         self._s3key = value
-        log.debug(str(self.__class__.__name__) + ".s3key = " + self._s3key)
+        log.debug(self._identify() + " = " + self._s3key)
 
     @property
     def size(self) -> float:
         if self._size is None:
             self._size = float(os.path.getsize(self.full_path))
-            log.debug(str(self.__class__.__name__) + ".size = " + str(self._size))
+            log.debug(self._identify() + " = " + str(self._size))
         return self._size
 
     @property
@@ -181,20 +158,54 @@ class LocalFile(object):
 
     @uploadable.setter
     def uploadable(self, value):
+
         if type(value) is bool:
             self._uploadable = value
+            log.debug(self._identify() + " = " + str(self._uploadable))
         else:
-            log.error("Trying to set variable to non-boolean type. Exiting...")
-            sys.exit(2)
-        log.debug(str(self.__class__.__name__) + ".uploadable = " + str(self._uploadable))
+            raise ValueError("Cannot set " + self._identify() + " to non-boolean type.")
+
+
+
+class CloudError(Exception):
+    """Base class for exceptions in this module."""
+
+    def __init__(self, message):
+        self.message = message
 
 
 class S3Bucket(object):
 
-    def __init__(self, name: str):
+    def __init__(self, name: str, region=None):
+
         self.name = name
-        self._client = boto3.client('s3')
+
+        if region is None:
+            self._client = boto3.client('s3')
+        else:
+            self._client = boto3.client('s3', region_name=region)
+
+        self._session = boto3.session.Session()
+        self._region = self._session.region_name
         self._transfer = boto3.s3.transfer.S3Transfer(self._client)
+        self._is_valid = False
+
+    def _identify(self):
+        return self.__class__.__name__ + "." + sys._getframe(1).f_code.co_name
+
+    def create(self, region=None) -> bool:
+
+        try:
+            if region is None:
+                self._client.create_bucket(Bucket=self.name)
+            else:
+                location = {'LocationConstraint': region}
+                self._client.create_bucket(Bucket=self.name, CreateBucketConfiguration=location)
+                self._region = region
+        except ClientError as e:
+            log.error(e)
+            return False
+        return True
 
     @property
     def name(self):
@@ -202,37 +213,122 @@ class S3Bucket(object):
 
     @name.setter
     def name(self, value):
-        self._name = value
-        log.debug(str(self.__class__.__name__) + ".name = " + self._name)
+
+        if self.valid_name(value):
+            self._name = value
+            log.debug(self._identify() + " = " + self._name)
+        else:
+            log.error(self._identify() + " = " + value)
+            raise ValueError("S3 Bucket name [" + value + "] is not valid.")
 
     def upload(self, file: LocalFile) -> float:
-        start = timer()
-        self._transfer.upload_file(file.full_path, self.name, file.s3key, extra_args=file.metadata, callback=self._progress(file, "Upload"))
-        end = timer()
+        if self.valid_bucket():
+            start = timer()
+            try:
+                self._transfer.upload_file(file.full_path,
+                                           self.name,
+                                           file.s3key,
+                                           extra_args=file.metadata,
+                                           callback=self._progress(file, "Uploading"))
+            except ClientError as e:
+                log.error(e)
+                return timer() - start
 
-        return end - start
+            return timer() - start
+        else:
+            raise CloudError("S3 Bucket [" + self.name + "] does not exist in AWS Region [" + self.region + "]")
 
     def metadata(self, key: str):
-        try:
-            metadata = self._client.head_object(Bucket=self.name, Key=key)['Metadata']
-        except botocore.exceptions.ClientError as e:
-            if e.response['Error']['Code'] == "404":
-                log.info(key + " does not exist in " + self.name)
-                return None
+        if self.valid_bucket():
+            try:
+                response = self._client.head_object(Bucket=self.name, Key=key)['Metadata']
+            except botocore.exceptions.ClientError as e:
+                if e.response['Error']['Code'] == "404":
+                    log.info(
+                        "Object [" + key + "] does not exist in S3 Bucket [" + self.name + "] in AWS Region [" + self.region + "]")
+                    return None
+                else:
+                    raise CloudError("Unknown return code from AWS: " + e.response['Error']['Code'])
+            else:
+                log.info("Object [" + key + "] was found in S3 Bucket [" + self.name + "] in AWS Region [" + self.region + "]")
+                return response
         else:
-            log.info(key + " found in " + self.name)
-            return metadata
+            raise CloudError("S3 Bucket " + self.name + " does not exist in AWS Region " + self.region)
 
+    def valid_bucket(self) -> bool:
+        if self._is_valid:
+            return True
+        else:
+            try:
+                self._client.head_bucket(Bucket=self.name)
+            except botocore.exceptions.ClientError as e:
+                if e.response['Error']['Code'] == "404":
+                    log.error("S3 Bucket [" + self.name + "] does not exist in AWS Region [" + self.region + "]")
+                    return False
+                else:
+                    raise CloudError("Unknown return code from AWS: " + e.response['Error']['Code'])
+            else:
+                self._is_valid = True
+                log.info("S3 Bucket [" + self.name + "] was found in AWS Region [" + self.region + "]")
+                return True
 
+    @property
+    def region(self):
+        return self._region
 
+    @property
+    def size(self):
+        if self.valid_bucket():
+            try:
+                response = self._client.list_objects(Bucket=self.name)['Contents']
+                bucket_size = sum(obj['Size'] for obj in response)
+                return bucket_size
+            except ClientError as e:
+                log.error("AWS responded with: " + e)
+                return 0
+        else:
+            raise CloudError("S3 Bucket [" + self.name + "] does not exist in AWS Region [" + self.region + "]")
 
+    @staticmethod
+    def valid_name(value: str) -> bool:
+
+        # Reduce textual repetition
+        log_prefix = "S3 Bucket name [" + str(value) + "]"
+
+        # Make sure input is a string
+        if not type(value) == str:
+            log.error(error_prefix + " is not a string")
+            return False
+
+        # The string must be between 3 and 63 characters long
+        if len(value) < 3:
+            log.error(log_prefix + " is too short: it must be more than 2 characters")
+            return False
+        if len(value) > 63:
+            log.error(log_prefix + " is too long: it must be fewer than 64 characters")
+            return False
+
+        # The first and last characters may not be a hyphen
+        if value.startswith("-") or value.endswith("-"):
+            log.error(log_prefix + " cannot begin or end with a hyphen")
+            return False
+
+        # All characters must be lowercase alphanumeric or a hyphen
+        valid_characters = re.compile(r'[^a-z0-9-]').search
+        if bool(valid_characters(value)):
+            log.error(log_prefix + " contains invalid characters")
+            return False
+        else:
+            log.info(log_prefix + " passed validation")
+            return True
+
+    # For large files it is useful to get progress updates
     @staticmethod
     def _progress(file: LocalFile, ops: str):
 
         _seen_so_far = 0
         _ops = ops
         _lock = threading.Lock()
-
         _msg_count = 0
         _msg_throttle = 50
 
@@ -242,10 +338,8 @@ class S3Bucket(object):
                 nonlocal _msg_count
                 _seen_so_far += bytes_amount
                 percentage = (_seen_so_far / file.size) * 100
-
-                if _msg_count % _msg_throttle == 0:
-                    log.info(f"{_ops}: {file.name}  {_seen_so_far} / {round(file.size)}  ({percentage:.2f}%)")
-
+                if _msg_count % _msg_throttle == 0 or int(percentage) == 100:
+                    log.info(f"{_ops} [{file.name}]  {_seen_so_far} / {round(file.size)}  ({percentage:.2f}%)")
                 _msg_count += 1
 
         return call
@@ -254,7 +348,18 @@ class S3Bucket(object):
 
 
 def fs_get_files(directory: str) -> list:
+    """
+    Gather list of files to upload to S3
+
+    :param directory: Root directory with files to upload to S3
+    :type directory: str
+    :return: returns a list of LocalFile objects
+    :rtype: list
+    """
+
+    # fix for macOS metadata files
     exclude_list: set[str] = {".DS_Store"}
+
     file_list: list = []
 
     for (path, dirs, files) in os.walk(directory):
@@ -264,9 +369,7 @@ def fs_get_files(directory: str) -> list:
 
     return file_list
 
-
 def main(argv):
-
     directory: str = ""
     bucket_name: str = ""
 
@@ -280,11 +383,11 @@ def main(argv):
     start_time = timer()
     file_sizes = []
 
+    # Parse the command line arguments
     try:
         opts, args = getopt.getopt(argv,
                                    "hd:b:fl:rs:t:",
                                    ["directory=", "bucket=", "file-limit=", "size-limit=", "time-limit="])
-
     except getopt.GetoptError:
         print(sys.argv[0] + " -d <base directory> -b <S3 bucket>")
         sys.exit(2)
@@ -304,7 +407,7 @@ def main(argv):
         elif opt in ("-l", "--file-limit"):
             try:
                 file_limit = int(arg)
-                log.debug("File limit set to: " + str(file_limit))
+                log.debug("File limit = " + str(file_limit))
             except ValueError:
                 log.error("File limit is not an integer")
                 sys.exit(2)
@@ -312,7 +415,7 @@ def main(argv):
         elif opt in ("-s", "--size-limit"):
             try:
                 size_limit = int(arg)
-                log.debug("Size limit set to: " + str(size_limit))
+                log.debug("Size limit = " + str(size_limit))
             except ValueError:
                 log.error("Size limit is not an integer")
                 sys.exit(2)
@@ -320,7 +423,7 @@ def main(argv):
         elif opt in ("-t", "--time-limit"):
             try:
                 time_limit = int(arg)
-                log.debug("Time limit set to: " + str(time_limit))
+                log.debug("Time limit = " + str(time_limit))
             except ValueError:
                 log.error("Time limit is not an integer")
                 sys.exit(2)
@@ -331,41 +434,53 @@ def main(argv):
         elif opt == "-r":
             random_shuffle = True
 
-    assert isinstance(bucket_name, str)
+    # Ensure a bucket name was specified
+    #assert isinstance(bucket_name, str)
     bucket = S3Bucket(bucket_name)
 
+    # Get a list of files to upload
     files = fs_get_files(directory)
+
+    # Randomize the list if desired
     if random_shuffle:
         random.shuffle(files)
 
+    # Process the list of files
     for file in files:
+        # First, verify several conditions are met before uploading
 
+        # Do not upload if the maximum number of files has been reached
         if len(file_sizes) >= file_limit:
             log.warning("File upload limit reached. Exiting...")
             break
 
+        # If files have already been uploaded, verify the upload size
+        # limit has not been reached (in bytes).
         if len(file_sizes) > 0:
-            total_data_uploaded = reduce(lambda x, y: round(x + y), file_sizes)
-            if size_limit > 0 and total_data_uploaded >= size_limit:
-                for msg in [str(round(x)) + " bytes" for x in file_sizes]:
-                    log.debug("Uploaded file of size " + msg)
-                log.debug("Total data uploaded = " + str(total_data_uploaded) + " bytes")
-                log.warning("Upload size limit reached. Exiting...")
-                break
+            # Don't bother to do calculations unless there's a limit
+            if size_limit > 0:
+                total_data_uploaded = reduce(lambda x, y: round(x + y), file_sizes)
+                if total_data_uploaded >= size_limit:
+                    for msg in [str(round(x)) + " bytes" for x in file_sizes]:
+                        log.debug("Uploaded file of size = " + msg)
+                    log.debug("Total data uploaded = " + str(total_data_uploaded) + " bytes")
+                    log.warning("Upload size limit reached. Exiting...")
+                    break
 
+        # Determine if the upload time limit has been reached (in seconds)
         elapsed_seconds = round(timer() - start_time)
-        log.debug("Elapsed time: " + str(elapsed_seconds) + " seconds")
-
+        log.debug("Elapsed time = " + str(elapsed_seconds) + " seconds")
         if elapsed_seconds >= time_limit:
             log.warning("Upload time limit reached. Exiting...")
             break
 
+        # Optionally use the relative file paths of the local files
+        # as the key for the S3 object (the default is to only use the
+        # name of the file).
         if use_folders:
             file.s3key = file.relative_path
 
-
-        log.debug("Using " + file.s3key + " as S3 object key.")
-
+        # Get the metadata of the file if it exists in S3
         s3_object_metadata = bucket.metadata(file.s3key)
 
         if s3_object_metadata is None:
@@ -373,22 +488,24 @@ def main(argv):
         else:
 
             if s3_object_metadata["sha256"] == file.hash:
-                log.info(file.s3key + " object exists in S3 with matching hash. Skipping...")
+                log.info("Object [" + file.s3key + "] in S3 Bucket [" + bucket.name + "] has matching hash. Skipping...")
             else:
-                log.info(file.s3key +
-                             " hash (" +
-                             file.hash +
-                             ") doesn't match S3 object (" +
-                             s3_object_metadata["sha256"] + ").")
+                log.info("File hash doesn't match Object hash in S3 Bucket")
+                log.debug("File " + file.s3key + " hash = " + file.hash)
+                log.debug("Object " + file.s3key + " hash = " + s3_object_metadata["sha256"])
 
                 file.uploadable = True
                 log.info("Uploading again...")
 
-
         if file.uploadable:
+            original_size = bucket.size
             elapsed_time = bucket.upload(file)
+            post_upload_size = bucket.size
+            percent = ((float(post_upload_size) / float(original_size)) - 1) * 100
             file_sizes.append(file.size)
-            log.info("Upload completed in " + str(elapsed_time) + " seconds.")
+            log.info("Upload completed in " + str(round(elapsed_time, 2)) + " seconds")
+            log.info("S3 Bucket size increased by " + str(round(percent, 2)) + "%")
+
 
 
 
